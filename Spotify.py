afb386d25e8ff024a06bc418c7de2d233800af4a
Cody <cke900@gmail.com>
2026-09-25 12:21:24 +0100

    Add csv files

---
+++ Spotify.py
@@ -0,0 +1,90 @@
+# python numbers
+import numpy as np
+
+import csv
+import json
+
+# data analysis library
+import pandas as pd
+
+with open("Liked_Songs.csv",mode="r" ,encoding="utf-8", newline="") as f:
+    data = csv.reader(f)
+    for row in data:
+        print(row)
+
+df = pd.read_csv('Liked_Songs.csv')
+
+
+
+
+
+
+
+# map to plot points
+# import matplotlib.pyplot as plt
+
+# python visualization library, based on matplotlib
+# import seaborn as sns 
+
+#SET UP JSON FILE TO CSV
+# json_data = 
+
+# # use pandas data to read dataset from csv
+# sp_data = pd.read_csv(
+#     "my_streaming_history.csv", on_bad_lines="skip"
+# )  # put spotify data tracks here
+# sp_tracks = pd.read_csv(
+#     "spotify_features_merged.csv", on_bad_lines="skip"
+# )  # put spotify features
+
+# n = 10
+
+# # viewing my data
+# print(sp_tracks.columns.tolist())
+# print(sp_data.columns.tolist())
+
+# # print(sp_data.head(n))  # not added yet
+# print("TOP 10")
+# print(sp_tracks[["Track Name", "Artist Name(s)", "Genres"]].head(n))  # tracks data
+
+# print(pd.isnull(sp_data).sum())  # return number of missing values in data
+
+# print(pd.isnull(sp_tracks).sum())  # return number of missing values in data
+# # print("TRACK INFO")
+# # print(sp_data.info())  # prints info from data frame
+
+# # print(sp_tracks.info())
+# # print("LEAST POPULAR SONGS I HOPE")
+# # # 10 least popular songs in spotify dataset
+# # a = sp_tracks.sort_values("Popularity", ascending=True)[
+# #     0:10
+# # ]  # 0:10 = first 10 rows, rows 0-10 by index
+# # print(a[["Track Name", "Popularity", "Artist Name(s)"]])  # [[]] = multiple columns
+
+# # # describes and flips so rows become columns
+# # # sp_data.describe().transpose()
+# # print("TRANSPOSE THING")
+# # sp_tracks.describe().transpose()
+# # print("COLUMNS FOR STREAMING")
+# # print(sp_data.head(10))
+# # print(sp_data.columns.toList())
+# # print("MOST PLAYED")
+# # play_counts = sp_data["master_metadata_track_name"].value_counts()
+# # print(play_counts.head(10))
+# # # a = sp_tracks
+# # # b = a[["popularity" > 90].sort_values("popularity", ascending=False)[:10]]
+# # # # 0:10 = first 10 rows, rows 0-10 by index
+# # # b[["name", "popularity", "artists"]]
+
+# # print("INDEX NIGHTMARE")
+# # # make release date column as index column
+# # print(sp_tracks.index[:5])
+# # print(type(sp_tracks.index[0]))
+# # rDate = sp_tracks
+# # rDate = sp_tracks.set_index("Release Date")
+# # rDate.index = pd.to_datetime(rDate.index, errors="coerce")
+# # rDate = rDate.sort_index(ascending=True)[0:10]
+# # print(rDate[["Name", "Artist Name(s)", "Genres"]].head(10))
+# # print(rDate.head(10))
+# # print(rDate.index.isna().sum())
+# # #TODO fix headers
